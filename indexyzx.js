fc7de71423189c2c14ee742cf1de5549c3b40d4c
nyizaeri <nyizaeri@gmail.com>
2024-05-20 23:46:52 +0800

    Add files via upload

---
+++ indexyzx.js
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Yiesha Nichole G. Ceriales</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa:700" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div id="loading">
+        <div id="spinner"></div>
+    </div>
+    <div id="header" class="animated slideInDown" style="animation-delay:1.8s;">
+    <div id="title">Yiesha Nichole G. Ceriales</div><br>
+    <div id="links">
+        <a href="#skills">skills</a>
+        <a href="#contact">contact</a>
+    </div>
+    </div>
+
+    <center>
+    <div id="middle">
+        <div id="tagline" class="animated zoomIn" style="animation-delay:1.8s;">
+            I am 14 years old and I am from Grade 9 - Earth. <br>
+            <button class="btn_one">haha ok</button>
+        </div>
+    </div>
+    </center>
+
+    <div id="portfolio">
+             <div id="about me">
+                 <h1>about me!</h1>   
+                 <div>
+                        <p>
+                            I can communicate and interact with other people in my community. I love to write my own stories as my form of expression. I know how to play some instruments such as guitar, flute, and ukulele. I also love to draw and paint. In my free time, I also like to watch films from different underground directors. I am a consistent honor student since elementary. 
+                        </p>
+                 </div> 
+
+             </div>
+         
+              <div id="contact">
+                 <h1>contact.</h1> 
+                 <form>
+                     <input type="text" placeholder="name" required>
+                     <input type="email" placeholder="email" required><br>
+                     <textarea placeholder="your message" required rows="5"></textarea><br>
+                     <button class="btn_one">send</button>
+                 </form> 
+             </div>
+    </div>
+    <div id="footer">
+        whatever will be, will be. <br><br>
+    </div>
+<script src="index.js" type="text/javascript"></script>
+</body>
+</html>
